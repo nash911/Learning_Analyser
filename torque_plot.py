2430c76863c72e26e4471b3e322823d2a4a10e3c
Avinash Ranganath <nash911@gmail.com>
2020-12-16 17:18:54 -0500

    Add individual axis (X, Y, Z) plots for torque plot

--- torque_plot.py
+++ torque_plot.py
@@ -13,11 +13,15 @@ file_time_counter = 0
 
 
 def main(argv):
+    trq = False
     avg_trq = False
     indiv_trq = False
     trq_rew = False
     joint_rew = False
     window_seconds = 5
+    x = False
+    y = False
+    z = False
 
     indiv_trq_only = False
     joint_rew_only = False
@@ -29,22 +33,22 @@ def main(argv):
               'turquoise', 'darkblue', 'beige', 'salmon', 'olive', 'hotpink',
               'darkred', 'sand', 'armygreen', 'darkgrey', 'crimson', 'eggplant', 'coral']
 
-    file_name = '/home/nash/DeepMimic/output/avg_torque.dat'
-
     try:
-        opts, args = getopt.getopt(argv, "haijrw:", ["avg_torque", "individual_torque",
-                                                     "joint_reward", "torque_reward",
-                                                     "window_size"])
+        opts, args = getopt.getopt(argv, "htajirxyzw:", ["torque", "avg_torque", "joint_reward",
+                                                         "individual_torque", "torque_reward", "X",
+                                                         "Y", "Z", "window_size"])
     except getopt.GetoptError:
-        print("torque_plot.py -a -i -j -r -w [--avg_torque --individual_torque --joint_reward "
-              "--torque_reward --window_size]")
+        print("torque_plot.py -t -a -i -j -r -w -x -y -z [--torque --avg_torque --individual_torque"
+              " --joint_reward --torque_reward --window_size --X --Y --Z]")
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print("torque_plot.py -a -i -j -r -w [--avg_torque --individual_torque --joint_reward "
-                  "--torque_reward --window_size]")
+            print("torque_plot.py -t -a -i -j -r -w -x -y -z [--torque --avg_torque "
+                  "--individual_torque --joint_reward --torque_reward --window_size --X --Y --Z]")
             sys.exit()
+        elif opt in ("-t", "--torque"):
+            trq = True
         elif opt in ("-a", "--avg_torque"):
             avg_trq = True
         elif opt in ("-i", "--individual_torque"):
@@ -55,6 +59,20 @@ def main(argv):
             trq_rew = True
         elif opt in ("-w", "--window_size"):
             window_seconds = int(arg)
+        elif opt in ("-x", "--X"):
+            x = True
+        elif opt in ("-y", "--Y"):
+            y = True
+        elif opt in ("-z", "--Z"):
+            z = True
+
+    if trq:
+        file_name = '/home/nash/DeepMimic/output/torque.dat'
+    else:
+        file_name = '/home/nash/DeepMimic/output/avg_torque.dat'
+
+    if not(x or y or z):
+        x = y = z = True
 
     if not (avg_trq or trq_rew or joint_rew) and indiv_trq:
         indiv_trq_only = True
@@ -64,13 +82,24 @@ def main(argv):
 
     window_size = 30 * window_seconds
     time_step = 0.033332
+    if trq:
+        window_size *= 20
+        time_step /= 20
 
     graph_data = open(file_name, 'r').read()
     lns = graph_data.split('\n')
-    data_dim = len(lns[1].split(' '))
+    if trq:
+        # data_dim = len(lns[1].split(':'))
+        data_dim = len(lns[1].split(':')) - 3
+
+        # joint_dims = [len(d.split(' ')) for d in data_dim]
+    else:
+        data_dim = len(lns[1].split(' '))
 
     if indiv_trq_only or joint_rew_only:
         num_plots = int((data_dim - 2) / 2)
+    elif trq:
+        num_plots = int(data_dim)
     else:
         num_plots = 0
         if avg_trq:
@@ -82,14 +111,67 @@ def main(argv):
         if joint_rew:
             num_plots += 1
 
-    fig, axs = plt.subplots(num_plots, sharey=(True if indiv_trq_only or joint_rew_only else False),
-                            sharex=True)
+    fig, axs = plt.subplots(num_plots, sharey=(True if indiv_trq_only or joint_rew_only or trq
+                                               else False), sharex=True)
     fig.suptitle('Torque Plot', fontsize=20)
 
     def plot_axs(sub_plot):
         return axs[sub_plot] if num_plots > 1 else axs
 
-    def animate(i):
+    def trq_animate(i):
+        global prev_file_mod_time
+        global file_time_counter
+
+        file_mod_time = os.stat(file_name)[8]
+        if file_mod_time == prev_file_mod_time:
+            file_time_counter += 1
+        else:
+            file_time_counter = 0
+        prev_file_mod_time = file_mod_time
+
+        if file_time_counter > 10:
+            return
+
+        graph_data = open(file_name, 'r').read()
+        lines = graph_data.split('\n')
+        t = list()
+
+        plot_data = np.empty((data_dim, window_size, 3))
+
+        for i, line in enumerate(lines[-window_size:]):
+            if len(line) > 1 and not line[0] == '#':
+                joints_data = line.split(':')
+                # for j, joint in enumerate(joints_data):
+                for j, joint in enumerate(joints_data[:-3]):
+                    data_point = joint.split(' ')
+                    if len(data_point) > 1:
+                        plot_data[j, i] = np.array([float(dp) for dp in data_point])
+                    else:
+                        plot_data[j, i, 2] = float(data_point[0])
+                t.append(float(i) * time_step)
+
+        for ax in axs:
+            ax.clear()
+
+        for i, j in enumerate(plot_data):
+            if x:
+                plot_axs(i).plot(t[-window_size:], j[:len(t), 0].tolist(), color='red',
+                                 label=('J-' + str(i+1) + '-X'))
+            if y:
+                plot_axs(i).plot(t[-window_size:], j[:len(t), 1].tolist(), color='blue',
+                                 label=('J-' + str(i+1) + '-Y'))
+            if z:
+                plot_axs(i).plot(t[-window_size:], j[:len(t), 2].tolist(), color='green',
+                                 label=('J-' + str(i+1) + '-Z'))
+            if i == np.ceil(num_plots/2):
+                plot_axs(i).set(ylabel='Torque')
+                # plot_axs(i).legend(bbox_to_anchor=(0.5, -0.08*num_plots), loc='upper center',
+                #                    ncol=5, prop={'size': 12})
+            plot_axs(i).legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 12})
+        plot_axs(num_plots-1).set(xlabel='Time(s)')
+        fig.subplots_adjust(right=0.85)
+
+    def all_animate(i):
         global prev_file_mod_time
         global file_time_counter
 
@@ -175,7 +257,10 @@ def main(argv):
             fig.subplots_adjust(bottom=0.15)
             sub_plot += 1
 
-    _ = animation.FuncAnimation(fig, animate, interval=100)
+    if trq:
+        _ = animation.FuncAnimation(fig, trq_animate, interval=100)
+    else:
+        _ = animation.FuncAnimation(fig, all_animate, interval=100)
     plt.show()
 
 
